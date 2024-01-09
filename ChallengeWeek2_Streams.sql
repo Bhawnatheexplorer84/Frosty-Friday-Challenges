19c37ee3d9542b504372fa0d9689e07ffec6dea4
Bhawnatheexplorer84 <146433046+Bhawnatheexplorer84@users.noreply.github.com>
2024-01-09 13:23:46 +0000

    Create ChallengeWeek2_Streams.sql

---
+++ ChallengeWeek2_Streams.sql
@@ -0,0 +1,17 @@
+/* Question from Week 2 Frosty Friday Challenge #2*/
+Question.... : https://frostyfriday.org/blog/2022/07/15/week-2-intermediate/  
+
+ //What are you required to do
+##The challenge asks us to load in parquet data and transform it into a table, then create a stream that will only show changes to the DEPT and JOB_TITLE columns##
+
+Lets first create the environment configuration:For this refer the Common environment created for all challenges
+  // Environment Configuration
+USE DATABASE Frosty_DB;
+USE WAREHOUSE COMPUTE_WH;
+USE SCHEMA FrostyCHALLENG;
+
+
+
+
+
+
